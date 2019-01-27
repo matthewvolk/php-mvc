a215fdf7d64b6f3bdc0d335804f2ec0ebac8d15d
Matthew Volk <volkmattj@gmail.com>
2019-01-26 16:29:34 -0800

    implement range generator function

--- functions.php
+++ functions.php
@@ -10,3 +10,13 @@ function connectToDb() {
       die();
   }
 };
+
+
+function generateRange($start, $end) {
+  echo "<strong>Generating range from $start to $end</strong>\n\n";
+  echo "<ul>";
+  for ($i = $start; $i <= $end; $i++) {
+    echo "<li>The number is: $i</li>";
+  }
+  echo "</ul>";
+}
