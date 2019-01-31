2beb580ca4e428bc293158623d4cc6588dd7df56
Matthew Volk <volkmattj@gmail.com>
2019-01-30 17:50:32 -0800

    rename folder

--- OOP/Task.php
+++ OOP/Task.php
@@ -2,11 +2,18 @@
 
 class Task {
   public $description;
+  public $completed = false;
 
   public function __construct($description) {
     $this->description = $description;
   }
+
+  public function complete($isComplete) {
+    $this->completed = $isComplete;
+  }
 }
 
 $task = new Task('Go to the store');
-var_dump($task->$description);
+var_dump($task);
+$task->complete(true);
+var_dump($task);
