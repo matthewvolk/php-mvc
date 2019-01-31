8c79b40de167e1f6465aaef0c0f348b6798437a3
Matthew Volk <volkmattj@gmail.com>
2019-01-30 20:26:36 -0800

    abstract classes and extending classes

--- OOP/Inheritance.php
+++ OOP/Inheritance.php
@@ -1,6 +1,23 @@
 <?php
 
-class Mother {
+/**
+ * Abstract classes are classes that will never be
+ * instantiated, but may hold methods & properties
+ * that should be inherited amongst subclasses
+ */
+abstract class Human {
+  protected $nationality;
+
+  public function __construct($nationality) {
+    $this->nationality = $nationality;
+  }
+
+  public function getNationality() {
+    return $this->nationality;
+  }
+}
+
+class Mother extends Human {
   public function getEyes() {
     return 2;
   }
@@ -10,4 +27,6 @@ class Child extends Mother {
 
 }
 
-(new Child)->getEyes();
+$mother = new Mother("American");
+var_dump($mother->getEyes());
+var_dump($mother->getNationality());
