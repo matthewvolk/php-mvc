22c26cea424c2fb5f2c04b72a503eb31e5f4b1a2
Matthew Volk <volkmattj@gmail.com>
2019-01-30 18:37:26 -0800

    instantiate and use class

--- OOP/LightSwitch.php
+++ OOP/LightSwitch.php
@@ -7,20 +7,25 @@
  * under the hood to make the light switch work
  */
 class LightSwitch {
- public function on() {
+  public function on() {
+    $this->connect();
+    print("Light switch ON");
+  }
 
- }
+  public function off() {
+    $this->connect();
+    print("Light switch OFF");
+  }
 
- public function off() {
-
- }
-
- /**
+  /**
   * Private vs. Protected
   * Private methods may ONLY be accessed from the parent class,
   * not from objects that extend the class
   */
- private function connect() {
-
- }
+  private function connect() {
+    print("Connecting to electricity...");
+  }
 }
+
+$light = new LightSwitch();
+$light->on();
